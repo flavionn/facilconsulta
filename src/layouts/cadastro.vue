3d8cdbf00d13fbc0e7171703b87689e392eb710c
Flávio Nogueira Nery <flavio8163@gmail.com>
2021-10-08 21:07:34 -0300

    update desktop style

--- src/layouts/cadastro.vue
+++ src/layouts/cadastro.vue
@@ -11,7 +11,7 @@ const subtitulo = computed(() => route.meta.subtitulo)
 
 <template lang="pug">
 
-layout-container(class="px-8 space-y-8 pt-12 pb-16")
+layout-container(class="px-8 space-y-8 pt-12 pb-16 mx-auto md:(max-w-2xl)")
 	ui-botao-voltar
 	ui-cadastro-titulo(v-if="titulo") {{ titulo }}
 	ui-cadastro-subtitulo(v-if="subtitulo") {{ subtitulo }}
